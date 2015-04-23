9109a4ee28fcec2f956f8f9a6021c7f89cbaba9b
mformlos <maud.formanek@univie.ac.at>
2015-04-23 13:12:30 +0200

    try n = 10000

--- star-polymers/src/star-polymers.cpp
+++ star-polymers/src/star-polymers.cpp
@@ -41,7 +41,7 @@ int main() {
 
 	clock_t begin = clock();
 
-	for (int n = 0; n < 4000; n++) {
+	for (int n = 0; n < 10000; n++) {
 		std::cout << n << " ";
 		//if (n == 18539) box.print_molecules(temp_file);
 		//if ( n > 1e5 && !(n%10000)) {
