50cb62f987ed34b15fde87c9f163bff92ce442e2
mformlos <maud.formanek@univie.ac.at>
2015-10-19 12:10:01 +0200

    params.py is now generating start configs for MPC runs

--- averages.py
+++ averages.py
@@ -10,23 +10,45 @@ equil = 0
 if len(sys.argv) > 1: 
     equil = float(sys.argv[1])
 
-for file in glob.glob(filenames): 
-    averages = np.zeros(15)
+for file in sorted(glob.glob(filenames)): 
+    averages = np.zeros(16)
     gyration = np.zeros((3,3))
     N = 0
+    N_patch = 0
+    S = 0
     stat = open(file, "r")
     print file
     for i, line in enumerate(stat): 
         data = line.split(" ")
         if float(data[0])>equil:   
             for j in range(15): 
+                if j == 4 and float(data[j+1]) > 0.0: 
+                    N_patch += 1   
                 averages[j] += float(data[j+1])
-
             N += 1
+            gyration[0,0] = float(data[7])
+            gyration[0,1] = float(data[8])  
+            gyration[0,2] = float(data[9])
+            gyration[1,0] = gyration[0,1]
+            gyration[1,1] = float(data[11])
+            gyration[1,2] = float(data[12])
+            gyration[2,0] = gyration[0,2]
+            gyration[2,1] = gyration[1,2]
+            gyration[2,2] = float(data[15])
+            eigenvalues, eigenvectors = np.linalg.eig(gyration)
+            eigenvalues = np.sort(eigenvalues) 
+            ev = eigenvalues[::-1]
+            I = ev[0]+ev[1]+ev[2]
+            averages[15] += (3.*ev[0]-I)*(3.*ev[1]-I)*(3.*ev[2]-I)/pow(I,3)
+
     stat.close()
 
-    for j in range(15): 
-        averages[j] /= float(N)
+    for j in range(16):
+        if j == 5: 
+            averages[j] /= float(N_patch) 
+        else:  
+            averages[j] /= float(N)
+
     gyration[0,0] = averages[6]
     gyration[0,1] = averages[7]  
     gyration[0,2] = averages[8]
@@ -38,9 +60,22 @@ for file in glob.glob(filenames):
     gyration[2,2] = averages[14]
 
     eigenvalues, eigenvectors = np.linalg.eig(gyration)
+    A = file.split('_A')
+    A = A[1].split('_')
+    A = float(A[0])
+    B = file.split('_B')
+    B = B[1].split('_')
+    B = float(B[0])
+    alpha = str(B/(A+B))
+    Arms = file.split('Arms')
+    Arms = Arms[1].split('_')
+    Arms = Arms[0]
+    Lambda = file.split('_Lambda')
+    Lambda = Lambda[1].split('_')
+    Lambda = Lambda[0]
 
-    f_output.write("%s" %file) 
-    for j in range(15):
+    f_output.write("%s %s %s" %(Arms, alpha, Lambda)) 
+    for j in range(16):
         f_output.write(" %f" %averages[j])
     for j in range(3): 
         f_output.write(" %f" %eigenvalues[j])
--- cumulative_average.py
+++ cumulative_average.py
@@ -14,18 +14,21 @@ n_output = str.replace(filename, "statistics", "cumulative_average")
 f_output = open(n_output, "w")
 
 # e_pot, r_gyr, number of patches, patch size
-average = np.zeros(4)
+average = np.zeros(5)
 N = 0
 f_input = open(filename, "r")
 for i, line in enumerate(f_input): 
+    if i == 0: 
+        continue
     data = line.split(" ") 
     average[0] += float(data[1])
-    average[1] += float(data[4]) 
-    average[2] += float(data[5])
-    average[3] += float(data[6])
+    average[1] += float(data[6]) 
+    average[2] += float(data[4])
+    average[3] += float(data[5])
+    average[4] += float(data[3])
     N += 1
     if (data[0] >= av_step): 
-        f_output.write("%i %f %f %f %f \n" %(int(data[0]), average[0]/N, average[1]/N, average[2]/N, average[3]/N)) 
+        f_output.write("%i %f %f %f %f %f \n" %(int(data[0]), average[0]/N, average[1]/N, average[2]/N, average[3]/N, average[4]/N)) 
         av_step += av_step
 
 f_output.close()
@@ -34,9 +37,9 @@ g('set title "' + n_output + '"')
 g('set ylabel "cumulative average"') 
 g('set xlabel "step"') 
 
-title = ["e_pot", "r_gyr", "n_patch", "s_patch"]
+title = ["e_pot", "r_gyr", "n_patch", "s_patch", "temperature"]
 
-for i in range(4): 
+for i in range(5): 
     plot_command = 'plot "' + n_output + '" u 1:' + str(i+2) + ' title "' + title[i] + '" '
     g('set term x11 ' + str(i))
     g(plot_command)
