94e7fbf46937f1e33688247af28d9958a72f5d52
mformlos <maud.formanek@univie.ac.at>
2016-04-25 12:40:03 +0200

    averages_dir updated for d_patch and cosines, plot.py added

--- results/averages_dir.py
+++ results/averages_dir.py
@@ -7,7 +7,7 @@ import sys
 directory = str(sys.argv[1])
 directory = directory.replace('/', '')
 f_output = open("./averages_" + directory+".dat", "w") 
-f_output.write("#1Arms  2Lambda  3Shear  4Epot  6Ekin  8Temp  10N_patch  12S_patch  14R_gyr  16G_xx  18G_xy  20G_xz  22G_yx  24G_yy  26G_yz  28G_zx  30Gzy  32Gzz  34wx  36wy  38wz \n")
+f_output.write("#1Arms  2Lambda  3Shear  4Epot  6Ekin  8Temp  10N_patch  12S_patch 14D_patch 16COS_patch 18R_gyr  20G_xx  22G_xy  24G_xz  26G_yx  28G_yy  30G_yz  32G_zx  34Gzy  36Gzz  38wx  40wy  42wz \n")
 filenames = "./"+str(sys.argv[1])+"statistics*"
 equil = 100000000
 
@@ -17,8 +17,8 @@ if len(sys.argv) > 2:
     equil = float(sys.argv[2])
 
 for file in sorted(glob.glob(filenames)): 
-    averages = np.zeros(19)
-    stdev = np.zeros(19)
+    averages = np.zeros(21)
+    stdev = np.zeros(21)
     gyration = np.zeros((3,3))
     N = 0
     N_patch = 0
@@ -30,34 +30,37 @@ for file in sorted(glob.glob(filenames)):
             continue
         data = line.split(" ")
         if float(data[0])>equil:   
-            for j in range(18): 
+            for j in range(20):
                 if j == 4 and float(data[j+1]) > 0.0: 
                     N_patch += 1   
                 averages[j] += float(data[j+1])
                 #stdev[j] += np.power(float(data[j+1]),2)
             N += 1
-            gyration[0,0] = float(data[7])
-            gyration[0,1] = float(data[8])  
-            gyration[0,2] = float(data[9])
+            gyration[0,0] = float(data[9])
+            gyration[0,1] = float(data[10])
+            gyration[0,2] = float(data[11])
             gyration[1,0] = gyration[0,1]
             gyration[1,1] = float(data[11])
             gyration[1,2] = float(data[12])
             gyration[2,0] = gyration[0,2]
             gyration[2,1] = gyration[1,2]
-            gyration[2,2] = float(data[15])
+            gyration[2,2] = float(data[17])
             eigenvalues, eigenvectors = np.linalg.eig(gyration)
             eigenvalues = np.sort(eigenvalues) 
             ev = eigenvalues[::-1]
             I = ev[0]+ev[1]+ev[2]
             temp = (3.*ev[0]-I)*(3.*ev[1]-I)*(3.*ev[2]-I)/pow(I,3)
-            averages[18] += temp
-            #stdev[18] += np.power(temp, 2) 
-    for j in range(19):
+            averages[20] += temp
+            #stdev[18] += np.power(temp, 2)
+    N_patch_single = averages[4]
+    for j in range(21):
         if j == 4: 
             #stdev[j] = np.sqrt((1/(N_patch-1))*(stdev[j] - (1/N_patch)*np.power(averages[j],2)))
-            averages[j] /= float(N_patch) 
+            averages[j] /= float(N_patch)
+        if j >= 5 and j <= 7:
+            averages[j] /= N_patch_single
 
-        else: 
+        else:
             #print stdev[j], np.power(averages[j], 2)/N 
             #stdev[j] = np.sqrt((1/(N-1))*(stdev[j] - (1/N)*np.power(averages[j],2)))
             averages[j] /= float(N)
@@ -69,14 +72,17 @@ for file in sorted(glob.glob(filenames)):
             continue
         data = line.split(" ")
         if float(data[0])>equil:   
-            for j in range(18): 
+            for j in range(20):
                 if j != 4 or float(data[j+1]) > 0.0:  
                     stdev[j] += np.power(float(data[j+1]) - averages[j],2)
   
-    for j in range(19): 
+    for j in range(21):
         if j == 4: 
             stdev[j] = np.sqrt(stdev[j]/((N_patch-1)*N_patch))
-        else: 
+        if j >= 5 and j <= 7:
+            stdev[j] = np.sqrt(stdev[j]/((N_patch_single-1)*N_patch_single))
+
+        else:
             #print stdev[j]
             stdev[j] = np.sqrt(stdev[j]/((N-1)*N))   
             #print stdev[j]               
---
+++ results/plot.py
@@ -0,0 +1,44 @@
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({'font.size':24})
+fig = plt.figure(figsize=(10,8), dpi =80)
+plt.subplot(1,1,1)
+
+filename = str(sys.argv[1])
+Arms = filename.split("Arms")[1]
+Arms = Arms.split(".dat")[0]
+
+Lambda, Shear, E_pot, N_patch, S_patch, D_patch, R_gyr = np.loadtxt(filename, unpack=True, usecols=(2, 3, 4, 10, 12, 14, 18))
+
+Lamda_datapoints = np.zeros(3)
+Lambdas = np.zeros(3)
+current_lambda = Lambda[0]
+Lambdas[0] = current_lambda
+i = 0
+for value in Lambda:
+    if value == current_lambda:
+        Lamda_datapoints[i] += 1
+    else:
+        i += 1
+        Lamda_datapoints[i] += 1
+        current_lambda == value
+        Lambdas[i] = current_lambda
+
+counter = 0
+for i in range(3):
+    Shear_temp = np.zeros(Lamda_datapoints[i])
+    E_pot_temp = np.zeros(Lamda_datapoints[i])
+    for j in range(Lamda_datapoints[i]):
+        Shear_temp[j] = Shear[counter]
+        E_pot_temp[j] = E_pot[counter]
+        counter += 1
+    labellam = "$\Lambda = " + Lambdas[i]
+    plt.plot(Shear_temp, E_pot_temp, label = labellam)
+
+
+
+
+
+
