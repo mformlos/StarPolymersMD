7a39493ed93b5f9593f5bfe266b1c049d6eb40ee
Maud <maud.formanek@univie.ac.at>
2016-04-27 01:11:54 +0200

    nicer colors

--- results/plot_MPC_tests.py
+++ results/plot_MPC_tests.py
@@ -3,6 +3,19 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
+###COLOURS###
+# These are the "Tableau 20" colors as RGB.    
+tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
+  
+# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
+for i in range(len(tableau20)):    
+    r, g, b = tableau20[i]    
+    tableau20[i] = (r / 255., g / 255., b / 255.)    
+
 #setting up the figure
 plt.rcParams.update({'font.size':20})
 plt.rc('text', usetex=True)
@@ -40,8 +53,8 @@ plt.xlabel(r"$y$ coordinate")
 plt.ylabel(r"average velocity $v_x$ in $x$-direction")
 
 #plot
-plt.plot(y, vyx, marker="o", color="green", linestyle="None")
-plt.plot(y, y*float(shear), color="black")
+plt.plot(y, vyx, marker="o", color=tableau20[16], linestyle="None")
+plt.plot(y, y*float(shear), color=tableau20[16])
 
 #save and show
 fig.savefig("velocity_profile_"+param_string+".pdf")
@@ -59,7 +72,7 @@ v_abs, prob = np.genfromtxt(maxwell_filename, unpack=True)
 plt.xlabel(r"$\vert \mathbf{v} \vert$")
 plt.ylabel(r"$P(\vert \mathbf{v} \vert)$")
 
-plt.plot(v_abs, prob, marker="o", markersize=3, color="blue", linestyle="None")
+plt.plot(v_abs, prob, marker="o", markersize=3, color=tableau20[16], linestyle="None")
 plt.plot(v_abs, ((1./(2.*np.pi))**(3./2.))*4.*np.pi*(v_abs**2)*np.exp(-v_abs**2/2), color="black")
 
 fig.savefig("maxwell_distribution_"+param_string+".pdf")
@@ -82,9 +95,9 @@ ck3 /= ck3[0]
 plt.xlabel(r"$t/\sqrt{ma^2/k_BT}$")
 plt.ylabel(r"$C_v^T (k,t)$")
 
-plt.semilogy(time, ck1, 'ro', label=r"$\mathbf{k} = (1,0,0)$" )
-plt.semilogy(time, ck2, 'bo', label=r"$\mathbf{k} = (0,1,0)$")
-plt.semilogy(time, ck3, 'go', label=r"$\mathbf{k} = (0,0,1)$")
+plt.semilogy(time, ck1, marker='o', color=tableau20[12], linestyle="None", label=r"$\mathbf{k} = (1,0,0)$" )
+plt.semilogy(time, ck2, marker='o', color=tableau20[16], linestyle="None", label=r"$\mathbf{k} = (0,1,0)$")
+plt.semilogy(time, ck3, marker='o', color=tableau20[18], linestyle="None", label=r"$\mathbf{k} = (0,0,1)$")
 plt.semilogy(time, np.exp(-(2.*np.pi/10.)**2*0.8705*time), color="black")
 fig.savefig("transverse_autocorr_"+param_string+".pdf")
 plt.show()
@@ -102,7 +115,7 @@ time, cv = np.genfromtxt(autocorr_filename, unpack=True)
 plt.xlabel(r"$t/\sqrt{ma^2/k_BT}$")
 plt.ylabel(r"$C_v(t)$")
 
-plt.loglog(time, cv, 'go')
+plt.loglog(time, cv, marker='o', color=tableau20[16], linestyle="None")
 
 fig.savefig("velocity_autocorr_"+param_string+".pdf")
 plt.show()
