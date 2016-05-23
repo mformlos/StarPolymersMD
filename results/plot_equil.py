1d8cd3873fce8b0a23c3f6cbdfbc1b874236ffd2
mformlos <maud.formanek@univie.ac.at>
2016-05-23 15:25:11 +0200

    plot_equil.py

---
+++ results/plot_equil.py
@@ -0,0 +1,79 @@
+import glob
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+###COLOURS###
+# These are the "Tableau 20" colors as RGB.
+tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+
+# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.
+for i in range(len(tableau20)):
+    r, g, b = tableau20[i]
+    tableau20[i] = (r / 255., g / 255., b / 255.)
+
+#setting up the figure
+plt.rcParams.update({'font.size':20})
+plt.rcParams.update({'text.latex.preamble': ['\usepackage{amssymb}', '\usepackage{amsmath}']})
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+
+fig = plt.figure(0, figsize=(10,8), dpi =80)
+plt.subplot(1,1,1)
+
+filename = str(sys.argv[1])
+
+
+Arm, Lambda, N_patch, N_patch_dev, S_patch, S_patch_dev, R_gyr, R_gyr_dev, S, S_dev, delta, delta_dev, c, c_dev = np.loadtxt(filename, unpack=True, usecols=(0,1, 9, 10, 11, 12, 13, 14, 39, 40, 41, 42, 43, 44))
+print Arm, Lambda
+
+characteristics = {'N_patch':[N_patch,N_patch_dev, r"$N_p$"], 'S_patch':[S_patch, S_patch_dev, r"$S_p$"], 'R_gyr':[R_gyr, R_gyr_dev, r"R_{gyr}"], 'S':[S, S_dev, r"$S$"], 'delta':[delta, delta_dev, r"$\delta$"], 'c':[c, c_dev, r"$c$"]}
+
+Arms_datapoints = np.zeros(3)
+Arms = np.zeros(3)
+Arm_color = [12, 16, 18]
+Arm_marker = ['x', 'd', 'o']
+current_arm = Arm[0]
+Arms[0] = current_arm
+i = 0
+for value in Arm:
+    if value == current_arm:
+        Arms_datapoints[i] += 1
+    else:
+        i += 1
+        Arms_datapoints[i] += 1
+        current_arm = value
+        Arms[i] = current_arm
+
+print Arms, Arms_datapoints
+
+for key, value in characteristics.iteritems():
+    counter = 0
+    for i in range(3):
+        Lambda_temp = np.zeros(Arms_datapoints[i])
+        data = np.zeros(Arms_datapoints[i])
+        data_dev = np.zeros(Arms_datapoints[i])
+        for j in range(int(Arms_datapoints[i])):
+            Lambda_temp[j] = Lambda[counter]
+            data[j] = value[0][counter]
+            data_dev[j] = value[1][counter]
+            counter += 1
+        labelarm = str(Arms[i])
+        plt.errorbar(Lambda_temp, data, yerr=data_dev, marker=Arm_marker[i], mew=1.5, markersize=10, color=tableau20[Arm_color[i]], label =r"$f= "+labelarm+"$")
+    plt.xlabel(r"$\lambda$")
+    plt.ylabel(value[2])
+    plt.legend(loc='best', numpoints = 1, prop={'size':20})
+    plt.show()
+
+
+
+
+
+
